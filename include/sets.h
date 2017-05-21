e8a928eb1c536e7be706b2e78e93e62b0c127d47
Ermakova Kate <ermakova_katya@bk.ru>
2017-05-21 23:39:36 +0300

    Add project

---
+++ include/sets.h
@@ -0,0 +1,9 @@
+#ifndef INCLUDE_SETS_H_
+#define INCLUDE_SETS_H_
+#include <vector>
+#include <utility>
+using std::vector;
+using std::pair;
+using std::make_pair;
+vector<int> Sets(int n, vector<pair<int, int>> pairs);
+#endif  // INCLUDE_SETS_H_
